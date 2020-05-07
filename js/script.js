fbe1cf872c05d4fe0cd4c71d1fcec72d8fa558e4
Glitch (itscodenation-clientproject) <none>
2020-05-07 21:35:42 +0000

    🕴🎺 Checkpoint ./js/script.js:3736814/60:8584864/98 ./index.html:8584864/34

--- js/script.js
+++ js/script.js
@@ -3,9 +3,14 @@ $("#genre").click(function() {
   let userInput = $("input").val();
   fetch("https://api.soundcloud.com/tracks?genres=" + userInput + "&client_id=5aa8e389ba4e24b6106af5159ab3e344")
   .then(function(response){
-    return response.json
+    return response.json();
   })
-  .then(function() {
-    
+  .then(function(info) {
+    // info = { tracks: [...] }
+    // info = [ ... ]
+    console.log(info);
+    info.forEach(function(song) {
+      $("")
+    })
   })
 });
